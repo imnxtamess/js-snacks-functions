35368a3d919d59e3826b8908eab0068c7be3662e
imnxtamess <davide.morisco.work@gmail.com>
2025-02-17 17:03:55 +0100

    add bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -10,9 +10,33 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+const date = new Date()
+const currentHour = date.getHours()
+
+function sayHello(hour, person) {
+  if (hour <= 13) {
+    console.log(`Buongiorno ${person}`);
+  }
+  else if (hour <= 17) {
+    console.log(`Buon pomeriggio ${person}`);
+  }
+  else if (hour <= 23) {
+    console.log(`Buonasera ${person}`);
+  }
+  else if (hour <= 4) {
+    console.log(`Buonanotte${person}`);
+  }
+  return
+}
+
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+
+console.log(sayHello(currentHour, name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
