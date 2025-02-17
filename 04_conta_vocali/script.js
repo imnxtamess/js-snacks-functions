3d04e36f3e7374e4c1a28812593c825b2e134006
imnxtamess <davide.morisco.work@gmail.com>
2025-02-17 16:02:51 +0100

    add 4th task

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,21 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function findVowels(input) {
+  const vowels = ["a", "e", "i", "o", "u"]
+  const inputVowels = []
+  for (let i = 0; i < input.length; i++) {
+    thisLetter = input[i]
+    if (vowels.includes(thisLetter)) {
+      inputVowels.push(thisLetter)
+    }
+  }
+  return inputVowels
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(findVowels(word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
